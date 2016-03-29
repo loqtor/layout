3e81e4aa32486ca69954002c85c03cea4bb9847e
Martin Martinez <martin@alphero.com>
2016-03-29 19:26:11 +1300

    Fix to the "intrinsic" measures thing.

--- js/src/layout.js
+++ js/src/layout.js
@@ -31,41 +31,79 @@
         },
 
         updateGallery = function(canvas) {
-          gallery.append(canvas);
+          gallery.append($(canvas).height(200));
         },
 
-        setImageHeight = function(layout, canvas) {
-          if(Layouts.isAll(layout.height.value)) {
-            return $(canvas).height();
+        // setImageHeight = function(layout, canvas) {
+        //   if(Layouts.isAll(layout.height.value)) {
+        //     return $(canvas).height();
+        //   }
+        //
+        //   return layout.height.value / layout.height.divider;
+        // },
+        //
+        // setImageWidth = function(layout, canvas) {
+        //   if(Layouts.isAll(layout.width.value)) {
+        //     return $(canvas).width();
+        //   }
+        //
+        //   return layout.width.value / layout.width.divider;
+        // },
+        //
+        // setImageCoordinates = function(canvas, layout, imageHeight, imageWidth, imageIndex) {
+        //   if(Layouts.isHorizontal(layout)) {
+        //     return {
+        //       x: imageIndex * imageHeight,
+        //       y: 0
+        //     }
+        //   } else if(Layouts.isVertical(layout)) {
+        //     return {
+        //       x: 0,
+        //       y: imageIndex * imageHeight
+        //     }
+        //   }
+        //
+        //   return {
+        //     x: 0,
+        //     y: 0
+        //   }
+        // },
+
+        setImageHeight = function(layout, targetCanvas) {
+          if(Layouts.isVertical(layout)) {
+            return $(targetCanvas).height() / images.length;
+          } else if(Layouts.isHorizontal(layout)) {
+            return $(targetCanvas).height();
+          } else if(layout.callback && layout.callback.constructor === 'Function') {
+            layout.callback();
           }
 
-          return layout.height.value / layout.height.divider;
+          return $(targetCanvas).height();
         },
 
-        setImageWidth = function(layout, canvas) {
-          if(Layouts.isAll(layout.width.value)) {
-            return $(canvas).width();
+        setImageWidth = function(layout, targetCanvas) {
+          if(Layouts.isVertical(layout)) {
+            return $(targetCanvas).width();
+          } else if(Layouts.isHorizontal(layout)) {
+            return $(targetCanvas).width() / images.length;
+          } else if(layout.callback && layout.callback.constructor === 'Function') {
+            layout.callback();
           }
 
-          return layout.width.value / layout.width.divider;
+          return $(targetCanvas).width();
         },
 
-        setImageCoordinates = function(canvas, layout, imageHeight, imageWidth, imageIndex) {
-          if(Layouts.isHorizontal(layout)) {
-            return {
-              x: imageIndex * imageHeight,
-              y: 0
-            }
-          } else if(Layouts.isVertical(layout)) {
+        setImageCoordinates = function(targetCanvas, layout, imageHeight, imageWidth, imageIndex) {
+          if(Layouts.isVertical(layout)) {
             return {
               x: 0,
-              y: imageIndex * imageHeight
-            }
-          }
-
-          return {
-            x: 0,
-            y: 0
+              y: imageHeight * imageIndex
+            };
+          } else if(Layouts.isHorizontal(layout)) {
+            return {
+              x: imageWidth * imageIndex,
+              y: 0
+            };
           }
         },
 
@@ -74,21 +112,21 @@
           layoutOptions.html('');
 
           for(var i = 0, layout; layout = layouts[i]; i++) {
-            var canvas = document.createElement(CANVAS),
-                context = canvas.getContext('2d');
+            var elem = $("<canvas>", {width:MAX_MEASURE, height:MAX_MEASURE});
+            var targetCanvas = elem[0];
+            targetCanvas.width = targetCanvas.height = MAX_MEASURE;
 
-            $(canvas).height(MAX_MEASURE);
-            $(canvas).width(150); // @Todo: Improve this
+            var context = targetCanvas.getContext('2d');
 
             for(var j = 0, image; image = images[j]; j++) {
-              var imageHeight = setImageHeight(layout, canvas),
-                  imageWidth = setImageWidth(layout, canvas),
-                  coordinates = setImageCoordinates(canvas, layout, imageHeight, imageWidth, j);
+              var imageHeight = setImageHeight(layout, targetCanvas),
+                  imageWidth = setImageWidth(layout, targetCanvas),
+                  coordinates = setImageCoordinates(targetCanvas, layout, imageHeight, imageWidth, j);
 
               context.drawImage(image._canvas, coordinates.x, coordinates.y, imageWidth, imageHeight);
             }
 
-            layoutOptions.append(canvas);
+            layoutOptions.append(targetCanvas);
           }
         },
 
@@ -110,6 +148,8 @@
         },
 
         updateView = function(canvas) {
+          $(canvas).height(MAX_MEASURE);
+          $(canvas).width(MAX_MEASURE);
           updateGallery(canvas);
           updateLayouts(canvas);
         },
