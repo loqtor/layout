80839be17e45d4e328beeb07122b95ee647f00b5
Martin Martinez <martin@alphero.com>
2016-04-14 20:16:52 +1200

    Adding the parameters to the overriding instructions comment.

--- js/src/custom.js
+++ js/src/custom.js
@@ -12,19 +12,19 @@
       */
       // ,
       // {
-      //   setImageMeasures: function() {
+      //   setImageMeasures: function (layout, targetCanvas, imageIndex) {
       //     return {
       //       height: 0,
       //       width: 0
       //     }
       //   },
-      //   setSourceCoordinates: function() {
+      //   setSourceCoordinates: function (canvas, layout, imageWidth, imageHeight, imageIndex) {
       //     return {
       //       x: 0,
       //       y: 0
       //     }
       //   },
-      //   setTargetCoordinates: function() {
+      //   setTargetCoordinates: function (targetCanvas, layout, imageWidth, imageHeight, imageIndex) {
       //     return {
       //       x: 0,
       //       y: 0
